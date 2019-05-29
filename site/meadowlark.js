d97e39e9bf8d169c0cf5d928a992fc5a2aa91bd7
Probhakar Roy <probhakarroy3110@gmail.com>
2019-05-29 20:04:13 +0530

    added handlebars

--- site/meadowlark.js
+++ site/meadowlark.js
@@ -2,31 +2,32 @@ var express = require('express');
 
 var app = express();
 
+var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res) => {
-    res.type('text/plain');
-    res.send('Main content here');
+    res.render('home');
 });
 
 app.get('/about', (req, res) => {
-    res.type('text/plain');
-    res.send('About Page');
+    res.render('about');
 });
 
 //Custom 404 Page
 app.use((req, res) => {
-    res.type('text/plain');
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
 //Custom 500 page
 app.use((err, req, res, next) => {
    console.error(err.stack);
-   res.type('text/plain');
    res.status(500);
-   res.send('500 - Server Error'); 
+   res.render('500'); 
 });
 
 app.listen(app.get('port'), () => {
