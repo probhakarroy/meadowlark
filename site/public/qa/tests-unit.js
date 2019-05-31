0b080776e510331616e8ed4a42463aafcb1c23d1
Probhakar Roy <probhakarroy3110@gmail.com>
2019-05-31 17:16:34 +0530

    created logic tests using mocha and chai.

---
+++ site/public/qa/tests-unit.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+var fortune = require('../../lib/fortune.js');
+var expect = require('chai').expect;
+
+suite('Fortune cookie tests', () => {
+    test('get_fortune() should return a fortune', () => {
+        expect(typeof fortune.get_fortune() === 'string');
+    });
+});
+
