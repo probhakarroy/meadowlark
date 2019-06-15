c22b99f340e05c98e687c267ff3455de92701f28
Probhakar Roy <probhakarroy3110@gmail.com>
2019-06-16 03:15:27 +0530

    * trying to integrate Travis CI.

---
+++ create_credentials_js.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+touch lib/credentials.js;
+cat > lib/credentials.js << EOL
+module.exports = {
+    cookie_secret: '${cookie_secret}',
+    gmail : {
+        user : '${gmail_user}',
+        password : '${gmail_password}',
+    },
+    gcs : {
+        project_id : '${gcs_pid}',
+        key: '${gcs_key}',
+    },
+    mongo : {
+        development : {
+            connection_string: '${mondo_dev}'
+        },
+        production : {
+            connection_string: '${mongo_production}'
+        },
+    }
+};
+EOL
